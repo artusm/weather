feaddecd300cdd180d0c2db5aa445753f6d65f9c
artusm <36174850+artusm@users.noreply.github.com>
2022-08-15 17:10:59 +0300

    Add alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import * as path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: { '@': path.resolve(__dirname, 'src/') },
+  },
   test: {
     globals: true,
     environment: 'jsdom',
