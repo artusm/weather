8b03443dde059b03c64bc23db862372af69b39b6
artusm <36174850+artusm@users.noreply.github.com>
2022-08-15 17:19:24 +0300

    Add vite inspect plugin

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,14 @@ import { defineConfig } from 'vite'
 
 import react from '@vitejs/plugin-react'
 import checker from 'vite-plugin-checker'
+import Inspect from 'vite-plugin-inspect';
 
 import * as path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
+    Inspect({ enabled: true }),
     react(),
     checker({ typescript: true })
   ],
